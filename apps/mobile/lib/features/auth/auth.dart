7c2d0c92241fa26e2b3b0208f0a52f51664c621d
Qunnnn <tranvanquan20211@gmail.com>
2026-04-20 15:00:31 +0700

    feat(auth): add barrel files

---
+++ apps/mobile/lib/features/auth/auth.dart
@@ -0,0 +1,8 @@
+export 'data/data_sources/index.dart';
+export 'data/models/index.dart';
+export 'data/repositories/index.dart';
+export 'domain/entities/index.dart';
+export 'domain/repositories/index.dart';
+export 'domain/use_cases/index.dart';
+export 'presentation/pages/index.dart';
+export 'presentation/providers/index.dart';
---
+++ apps/mobile/lib/features/auth/data/data_sources/index.dart
@@ -0,0 +1,1 @@
+export 'auth_mock_data_source.dart';
