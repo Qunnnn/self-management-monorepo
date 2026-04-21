b95c54590d622ff28ddf54c1054692a35b43fa98
Qunnnn <tranvanquan20211@gmail.com>
2026-04-21 18:16:19 +0700

    fix: correct AppRiverpodObserver import in main.dart

--- apps/mobile/lib/main.dart
+++ apps/mobile/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'app.dart';
-import 'core/providers/app_riverpod_observer.dart';
+import 'core/observers/app_riverpod_observer.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
